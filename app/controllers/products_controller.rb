bcbad21293df5c5b07d7184275c9896e640f7e3f
Misha Petrov <modesau@gmail.com>
2018-05-04 15:22:55 -0700

    add edit and delete methods for products. add flash messages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,6 +30,29 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+      flash[:notice] = "Product updated."
+    else
+      render :edit
+      flash[:notice] = "Something went wrong. Try again."
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to '/'
+    flash[:notice] = "Product deleted."
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price)
