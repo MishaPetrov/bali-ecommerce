13eafa34e795c3a291e1456a9425ad6c92536a61
Misha Petrov <modesau@gmail.com>
2018-05-04 14:15:16 -0700

    add flas messages for user session changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,17 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      flash[:notice] = "Sign in successful!"
       redirect_to '/'
     else
+      flash[:notice] = "Something went wrong! Try again!"
       redirect_to '/sign_in'
     end
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:notice] = "Sign out successful!"
     redirect_to '/sign_in'
   end
 
