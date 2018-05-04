91830817ef5c4aa79feea7005cb5bc4ddc1c3653
Misha Petrov <modesau@gmail.com>
2018-05-04 15:47:22 -0700

    add validations for user and product

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :orders
+  validates :email, :presence => true
+  validates :password_digest, :presence => true
 
   def previous_orders
     self.orders.where(status: 2).includes(order_items: :product)
