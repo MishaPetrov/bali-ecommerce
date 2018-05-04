6bb1710138ff8fca397be70e6c9c950bb51747f2
Misha Petrov <modesau@gmail.com>
2018-05-04 16:47:22 -0700

    add styling to products page

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   validates :name, :presence => true
   validates :description, :presence => true
   validates :price, :presence => true
+  validates :price, :numericality => { :greater_than => 0 }
 end
